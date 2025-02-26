282fbcef87db6e7b092055b2af360e7a7378e7cd
Will Delorm <willdelorm@gmail.com>
2025-02-25 21:23:03 -0800

    remove revalidation request function

--- utils/actions.utils.js
+++ utils/actions.utils.js
@@ -1,16 +1,15 @@
 "use server";
 
 import {
+  createResourceDocument,
+  deleteDocumentFromCollection,
+} from "./firebase.utils";
+import {
   announcementSchema,
   eventSchema,
   meetingSchema,
   resourceSchema,
 } from "./schemas.utils";
-import {
-  createResourceDocument,
-  deleteDocumentFromCollection,
-} from "./firebase.utils";
-import { revalidatePath } from "next/cache";
 
 export async function onSubmitResourceAction(collectionKey, data) {
   let schemaToParse;
@@ -57,29 +56,3 @@ export async function deleteEntry(collection, id) {
   const response = await deleteDocumentFromCollection(collection, id);
   return response;
 }
-
-export const forceCacheRevalidation = (revalidationRequest) => {
-  switch (revalidationRequest) {
-    case "resources":
-      revalidatePath("/resources", "layout");
-      revalidatePath("/dashboard/resources", "page");
-      break;
-    case "announcements":
-      revalidatePath("/", "page");
-      revalidatePath("/dashboard/announcements", "page");
-      break;
-    case "events":
-      revalidatePath("/programs/events", "page");
-      revalidatePath("/programs/one-stop-shop", "page");
-      revalidatePath("/dashboard/events", "page");
-      break;
-    case "meetings":
-      revalidatePath("/about/bylaws-&-minutes", "page");
-      revalidatePath("/dashboard/meetings", "page");
-      break;
-    default:
-      revalidatePath("/", "layout");
-  }
-  revalidatePath("/dashboard", "page");
-};
-
